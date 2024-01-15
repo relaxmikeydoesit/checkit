d505b2544d8787537a8cbfd6602de2d6cdf93314
Mikey Lee <124496222+relaxmikeydoesit@users.noreply.github.com>
2024-01-15 16:59:14 -0500

    added integration to boredAPI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Segment } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 import styles from '@/styles/Footer.module.css';
 
 export default function Footer() {
 
     return (
     <>
-    <Segment basic color='black' className={styles.footer}>
-    <p>© 2024 MIKEY LEE. All rights reserved. </p>
-    <div data-tf-live="01HK8DH54CN4TGX59AK8V2SF8N"></div><script src="//embed.typeform.com/next/embed.js"></script>
-    </Segment>
-      
+    <div basic color='black' className={styles.footer}>
+    <div className={styles.footerText}>
+        <p>&copy;2024 Mikey Lee. All rights reserved.<br />How Did I Do?</p>
+    <div data-tf-live="01HK8DH54CN4TGX59AK8V2SF8N"></div><script src="//embed.typeform.com/next/embed.js"></script> 
+    </div>
+    <Image href="https://relaxmikeydoesit.tech" src='relaxmikeydoesitfooter.png'/>
+    <Image href="https://github.com/relaxmikeydoesit/" src='github-mark-white.png' size="small"/>
+    </div>
     </>
     );
 }
